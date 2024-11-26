cbbf34db5e6ca5b611b79af255f071077eb99df4
MadjarovIvan <123532943+MadjarovIvan@users.noreply.github.com>
2024-11-26 20:45:03 +0100

    Created a routen system for the page and completed the sign in, sign up, admin and home page visualy

--- Blog/app/pages/signup.php
+++ Blog/app/pages/signup.php
@@ -153,26 +153,37 @@
         
         <main class="form-signin w-100 m-auto">
             <form>
-                <img class="mb-4" src="assets/images/logo.png" alt="" width="72" height="57">
-                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+                <a href="home">
+                    <img class="mb-4" src="assets/images/logo.png" alt="" width="72" height="57">
+                </a>
+                <h1 class="h3 mb-3 fw-normal">Create Account</h1>
 
                 <div class="form-floating">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Email address</label>
+                    <input name="username" type="text" class="form-control" id="floatingInput" placeholder="Username">
+                    <label for="floatingInput">Username</label>
                 </div>
                 <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                    <label for="floatingPassword">Password</label>
+                    <input name="email" type="email" class="form-control" id="floatingPassword" placeholder="name@example.com">
+                    <label for="floatingPassword">Email</label>
+                </div>
+
+                <div class="form-floating">
+                    <input name="password" type="password" class="form-control" id="floatingInput" placeholder="Password">
+                    <label for="floatingInput">Password</label>
+                </div>
+                <div class="form-floating">
+                    <input name="retype-password=" type="password" class="form-control" id="floatingPassword" placeholder="Retype Password">
+                    <label for="floatingPassword">Retype Password</label>
                 </div>
 
                 <div class="form-check text-start my-3">
-                    <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+                    <input class="form-check-input" name="terms" type="checkbox" value="terms" id="flexCheckDefault">
                     <label class="form-check-label" for="flexCheckDefault">
-                        Remember me
+                        Accept terms and conditions
                     </label>
                 </div>
                 <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-                <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
+                <p class="mt-5 mb-3 text-body-secondary">&copy; <?php echo date('Y') ?></p>
             </form>
         </main>
     <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
--- Blog/public/index.php
+++ Blog/public/index.php
@@ -1,4 +1,17 @@
 <?php 
+define("ROOT", 'http://localhost:8888/AdditionalProjects/Blog/public');
 
-echo "Home Page";
+$url = $_GET['url'] ?? 'home';
+$url = explode("/", $url);
+
+$page_name = trim($url[0]);
+$filename = "../app/pages/".$page_name.".php";
+
+if(file_exists($filename)){
+    require_once $filename;
+} else {
+    require_once "../app/pages/404.php";
+}
+
+
 
